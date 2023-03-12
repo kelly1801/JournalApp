a533bed62f8ef335954a56665ee363efe570c625
Kelly Chara <fabianachara@gmail.com>
2023-03-11 22:02:20 -0500

    add MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from './theme'
 const JournalApp = () => {
   return (
-    <>
+    <AppTheme>
       <AppRouter />
-    </>
+    </AppTheme>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import JournalApp from "./App";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
